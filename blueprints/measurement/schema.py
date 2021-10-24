a729d82e4b26256c994900383c4ca9d9b2fc4ab3
Khrystyna <khrystynavysotska@gmail.com>
2021-10-24 18:09:12 +0300

    Removed unnecessary nested object

--- blueprints/measurement/schema.py
+++ blueprints/measurement/schema.py
@@ -1,12 +1,9 @@
 from resources import ma
-from blueprints.float_sensor.schema import FloatSensorSchema
 
 
 class MeasurementSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'timestamp', 'water_level_in_metres', 'float_sensor_id', 'float_sensor')
-
-    float_sensor = ma.Nested(FloatSensorSchema)
+        fields = ('id', 'timestamp', 'water_level_in_metres', 'float_sensor_id')
 
 
 measurement_schema = MeasurementSchema()
